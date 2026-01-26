14d85d07b143b1491efdefd76c57da5f7487995a
高航 <1365624215@qq.com>
2026-01-26 16:53:40 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard 900991bf0fde6284bad629d2ff66777b6eaa41f9 Merge pull request #49 from SArthurX/fix-render-driver

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,11 +1,18 @@
 #include <Vtop.h>
-#include <stdint.h>
+#include <nvboard.h>
 
+#include <stdint.h>
+static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* top);
 
 int main() {
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
     Vtop* top = new Vtop;
 	while(1){
-		top -> eval;
+		dut.eval();
+		nvboard_update();
 	}
+	nvboard_quit();
     return 0;
 }
